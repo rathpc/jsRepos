103ae8efeccb6e9b5c230ab32409f06878d548a2
RathPC <rathpc@users.noreply.github.com>
2016-04-15 12:57:06 -0400

    Added a new function
    Added a combine/sort array function

--- arrayFns/arrayFns.js
+++ arrayFns/arrayFns.js
@@ -79,3 +79,77 @@ var arrayAnagramCompare = function (arr1,arr2) {
 		return "The array lengths do not match!";
 	}
 };
+
+//Combine two arrays and return a single sorted array
+//Case 1
+//Result = [1, 2, 3, 4, 5, 6]
+//var a1 = [1, 3, 5];
+//var a2 = [2, 4, 6];
+
+//Case 2
+//Result = [1, 1, 2, 2, 3, 3]
+//var a1 = [1, 2, 3];
+//var a2 = [1, 2, 3];
+
+//Case 3
+//Result = [1, 2, 3];
+//var a1 = [1, 2, 3];
+//var a2 = [];
+
+//Case 4
+//Result = [1, 2, 3, 4, 5, 6];
+//var a1 = [1, 5];
+//var a2 = [2, 3, 4, 6];
+
+var arrayFn = function(arg1, arg2) {
+	var tmp1 = arg1.length + arg2.length;
+	var tmp2 = [];
+	var sortedArray = [];
+	var counter1 = 0;
+	var counter2 = 0;
+	for(var i = 0; i < tmp1; i++) {
+		console.log(i+'|'+counter1+'|'+counter2);
+		if(arg1.length == 0 && arg2.length == 0) {
+			break;
+		} else if(arg1.length == 0 && arg2.length != 0) {
+			sortedArray = sortedArray.concat(arg2);
+			break;
+		} else if(arg2.length == 0 && arg1.length != 0) {
+			sortedArray = sortedArray.concat(arg1);
+			break;
+		} else if(arg1[counter1] < arg2[counter2]) {
+			sortedArray.push(arg1[counter1]);
+			counter1++;
+			if(counter1 == arg1.length) {
+				tmp2 = arg2.slice(counter2, arg2.length);
+				sortedArray = sortedArray.concat(tmp2);
+				break;
+			}
+		} else if(arg2[counter2] < arg1[counter1]) {
+			sortedArray.push(arg2[counter2]);
+			counter2++;
+			if(counter2 == arg2.length) {
+				tmp2 = arg1.slice(counter1, arg1.length);
+				sortedArray = sortedArray.concat(tmp2);
+				break;
+			}
+		} else if(arg1[counter1] == arg2[counter2]){
+			sortedArray.push(arg1[counter1]);
+			sortedArray.push(arg2[counter2]);
+			counter1++;
+			counter2++;
+			if(counter1 == arg1.length && counter1 < arg1.length) {
+				tmp2 = arg2.slice(counter2, arg2.length);
+				sortedArray = sortedArray.concat(tmp2);
+				break;
+			} else if(counter2 == arg2.length && counter2 < arg2.length) {
+				tmp2 = arg1.slice(counter1, arg1.length);
+				sortedArray = sortedArray.concat(tmp2);
+				break;
+			} else if(counter1 == arg1.length && counter2 == arg2.length) {
+				break;
+			}
+		}
+	}
+	return sortedArray;
+};
